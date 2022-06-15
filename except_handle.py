12705dc58772011359ec79298e800d5821d6fad4
taolli <1074591911>
2022-06-15 14:32:20 +0800

    test

--- except_handle.py
+++ except_handle.py
@@ -9,14 +9,24 @@ except ValueError:
 # 可以捕获多个异常
 # except (ValueError, AttributeError, KeyError, NameError):  多个异常时写出元祖的格式
 try:
-    print(1/0)
+    print(1 / 0)
 except ZeroDivisionError as e:
     print('0不能做除数 %s' % e)
-
 
 try:
     print(1 / a)
 except Exception as e:
     print('%s' % e)
-    
 
+# 自己定义错误的提示信息，在python中用raise
+try:
+    raise NameError('hello error')
+except NameError:
+    print('hi error')
+
+try:
+    a = open('name.txt')
+except Exception as e:
+    print(e)
+finally:
+    a.close()
